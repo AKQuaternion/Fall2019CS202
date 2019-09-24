3fdab2fa7d3f06e8c8de64fa2e32cf87ee07395b
Chris Hartman <chris.m.hartman@gmail.com>
2019-09-24 09:50:47 -0800

    Use a function to return from instead of a boolean flag

--- lab04/lab04.cpp
+++ lab04/lab04.cpp
@@ -13,41 +13,38 @@ using std::istringstream;
 #include <fstream>
 using std::ofstream;
 
-void getInput() {
-	int total = 0;
-	bool keepGoing = true;
-	while (keepGoing) {
-		int x;
-		cout << "Enter a number or more on one line, negative to quit. ";
-		string userInput;
-		getline(cin, userInput);
-		istringstream uin(userInput);
-		while (true) {
-			uin >> x;
-			if (!uin) {
-				if (uin.eof()) {
-					break;
-				}
-				cout << "Improper input, ignoring rest of line.\n";
-				uin.clear();
-				uin.ignore(999, '\n');
-				continue;
-			}
-			if (x < 0) {
-				keepGoing = false;
-				break;
-			}
-			total += x;
-		}
-	}
-
-	cout << "Your total was " << total << endl;
+int getInput() {
+  int total = 0;
+  while (true) {
+    int x;
+    cout << "Enter a number or more on one line, negative to quit. ";
+    string userInput;
+    getline(cin, userInput);
+    istringstream uin(userInput);
+    while (true) {
+      uin >> x;
+      if (!uin) {
+        if (uin.eof()) {
+          break;
+        }
+        cout << "Improper input, ignoring rest of line.\n";
+        uin.clear();
+        uin.ignore(999, '\n');
+        continue;
+      }
+      if (x < 0)
+        return total;
+      total += x;
+    }
+  }
 }
 
 int main()
 {
-	ofstream ofile("output.txt",std::ios::app);
-	ofile << "Hi world.\n";
-	return 0;
+//  ofstream ofile("output.txt",std::ios::app);
+//  ofile << "Hi world.\n";
+  int total = getInput();
+  cout << "Your total was " << total << endl;
+  return 0;
 }
 
