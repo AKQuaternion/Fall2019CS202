01411d410f039539e2e6c8a2336a86e1c9c6f353
Chris Hartman <chris.m.hartman@gmail.com>
2019-10-08 10:02:53 -0800

    Example of local static variable

--- EnumAndStatic/main.cpp
+++ EnumAndStatic/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+using std::cout;
+using std::endl;
+
+void foo() {
+  int y;
+  static int x=0;
+  cout << ++x << endl;
+  y = 13;
+}
+
+void bar() {
+  int f=55;
+  cout << "In bar, f is " << f << endl;
+}
 
 int main() {
+  foo();
+  bar();
+  foo();
   std::cout << "Hello, World!" << std::endl;
-  return 0;
 }
