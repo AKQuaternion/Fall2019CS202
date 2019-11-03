21fda61bc8a5545818e78fd5a0293516146e62f7
Chris Hartman <chris.m.hartman@gmail.com>
2019-11-03 13:48:07 -0900

    Made factory

--- Polymorphism/Shape.cpp
+++ Polymorphism/Shape.cpp
@@ -3,3 +3,31 @@
 //
 
 #include "Shape.hpp"
+#include <iostream>
+using std::cout;
+#include <random>
+
+void Circle::describe() { cout << "I am a circle.\n"; }
+
+void Square::describe() { cout << "I am a square.\n"; }
+
+std::unique_ptr<Shape> getShape(char c) {
+  static std::random_device r(time(0));
+  static std::default_random_engine e1(r());
+  static std::uniform_int_distribution<int> flip(0, 1);
+
+  switch (c) {
+  case 's':
+  case 'S':
+    return std::make_unique<Square>();
+  case 'c':
+  case 'C':
+    return std::make_unique<Circle>();
+  case 'r':
+  case 'R':
+    if (flip(e1) == 0)
+      return std::make_unique<Square>();
+    else
+      return std::make_unique<Circle>();
+  }
+}
--- Polymorphism/Shape.hpp
+++ Polymorphism/Shape.hpp
@@ -5,4 +5,26 @@
 #ifndef POLYMORPHISM_SHAPE_HPP
 #define POLYMORPHISM_SHAPE_HPP
 
+#include <memory> // for unique_ptr and make_unique
+
+class Shape {
+public:
+  virtual void describe() = 0; // pure virtual function
+};
+
+class Circle : public Shape {
+public:
+  void describe() override; // describe is virtual in the base class, so it
+  // is virtual here. You do not need to repeat the virtual keyword. But you
+  // should use the "= override" as above. This will let the compiler warn you
+  // if you do something wrong. What happens if you make the above function
+  // const (as it really should be!?)
+};
+
+class Square : public Shape {
+public:
+  void describe() override;
+};
+
+std::unique_ptr<Shape> getShape(char c);
 #endif // POLYMORPHISM_SHAPE_HPP
