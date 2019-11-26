1b877dd85317befcfa44a24148533004537a7abd
Chris Hartman <chris.m.hartman@gmail.com>
2019-11-26 10:01:25 -0900

    Initial commit

--- IntermediateSTL/main.cpp
+++ IntermediateSTL/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+using std::cout;
+using std::endl;
+#include <deque>
+#include <list>
+#include <vector>
+using std::deque;
+using std::list;
+using std::vector;
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  list<int> v{1, 2, 3, 4, 5};
+  auto start = v.begin();
+  ++start;
+  auto finish = v.end();
+  --finish;
+  for (auto it = start; it != finish; ++it)
+    cout << *it << " ";
+  cout << endl;
+  start = v.insert(start, 13);
+  for (auto it = start; it != finish; ++it)
+    cout << *it << " ";
+  cout << endl;
   return 0;
 }
